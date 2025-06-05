c842e414cba0a3cd5ffdac5f97ab6322fa65690a
Sk Sazid <1.sksazid@gmail.com>
2025-06-06 02:42:23 +0600

    v1.0 project350

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Project350
+# Project350 
+
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
 
+
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on Port 3000');
 });
